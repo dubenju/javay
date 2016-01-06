be1391ad588201e2b3fd49ec0980e3f6f3644cde
dubenju <dubenju@163.com>
2016-01-06 21:29:17 +0900

    condition

--- src/java/javay/awt/event/CalcultorFSM.java
+++ src/java/javay/awt/event/CalcultorFSM.java
@@ -7,13 +7,14 @@ import javay.fsm.FiniteStateMachine;
 import javay.fsm.state.State;
 import javay.fsm.state.StateFinal;
 import javay.fsm.state.StateInitial;
+import javay.fsm.transition.Condition;
 import javay.fsm.transition.Transition;
 
 public class CalcultorFSM implements FiniteStateMachine, Runnable {
 
 	private List<State> states;
 	private State initialState;
-	private List<Transition> transitions;
+	private List<List<Transition>> transitions;
 	private State currentState;
 
 	public CalcultorFSM() {
@@ -38,33 +39,62 @@ public class CalcultorFSM implements FiniteStateMachine, Runnable {
 		this.states.add(state7);
 		this.states.add(state8);
 
-		// TODO:要用邻接表(DAG)
-		this.transitions = new ArrayList<Transition>();
+		// 邻接表,逆临接表(DAG)
+		this.transitions = new ArrayList<List<Transition>>(this.states.size());
 		Transition tran_i_1 = new CalcultorTransition(this.initialState, null, null, null, state1);
+
+		// 初期状态 + 0.9 = 数值输入中
 		Transition tran_1_3 = new CalcultorTransition(state1, null, null, null, state3);
+		// 初期状态 + OPT = 操作符号输入完了
 		Transition tran_1_5 = new CalcultorTransition(state1, null, null, null, state5);
+		// 数值输入中 + OPT or = = 得出结果
 		Transition tran_3_4 = new CalcultorTransition(state3, null, null, null, state4);
+		// 得出结果 + 0.9 = 数值输入中
 		Transition tran_4_3 = new CalcultorTransition(state4, null, null, null, state3);
+		// 得出结果 + OPT = 操作符号输入完了
 		Transition tran_4_5 = new CalcultorTransition(state4, null, null, null, state5);
+		// 得出结果 + = 
 		Transition tran_4_7 = new CalcultorTransition(state4, null, null, null, state7);
+		// 操作符号输入完了 + OPT1 = 得出结果
 		Transition tran_5_4 = new CalcultorTransition(state5, null, null, null, state4);
+		// 操作符号输入完了 + 0.9 = 第二个数值输入中
 		Transition tran_5_6 = new CalcultorTransition(state5, null, null, null, state6);
+		// 第二个数值输入中 + OPT1 = 得出结果
 		Transition tran_6_4 = new CalcultorTransition(state6, null, null, null, state4);
+		// 第二个数值输入中 + OPT2 = 得出结果
 		Transition tran_6_5 = new CalcultorTransition(state6, null, null, null, state5);
+		//  + = 得出结果
 		Transition tran_7_5 = new CalcultorTransition(state7, null, null, null, state5);
 
-		this.transitions.add(tran_i_1);
-		this.transitions.add(tran_1_3);
-		this.transitions.add(tran_1_5);
-		this.transitions.add(tran_3_4);
-		this.transitions.add(tran_4_3);
-		this.transitions.add(tran_4_5);
-		this.transitions.add(tran_4_7);
-		this.transitions.add(tran_5_4);
-		this.transitions.add(tran_5_6);
-		this.transitions.add(tran_6_4);
-		this.transitions.add(tran_6_5);
-		this.transitions.add(tran_7_5);
+		List<Transition> ti = new ArrayList<Transition>();
+		ti.add(tran_i_1);
+		this.transitions.add(ti);
+		
+		List<Transition> t1 = new ArrayList<Transition>();
+		t1.add(tran_1_3);
+		t1.add(tran_1_5);
+		this.transitions.add(t1);
+		List<Transition> t2 = new ArrayList<Transition>();
+		this.transitions.add(t2);
+		List<Transition> t3 = new ArrayList<Transition>();
+		t3.add(tran_3_4);
+		this.transitions.add(t3);
+		List<Transition> t4 = new ArrayList<Transition>();
+		t4.add(tran_4_3);
+		t4.add(tran_4_5);
+		t4.add(tran_4_7);
+		this.transitions.add(t4);
+		List<Transition> t5 = new ArrayList<Transition>();
+		t5.add(tran_5_4);
+		t5.add(tran_5_6);
+		this.transitions.add(t5);
+		List<Transition> t6 = new ArrayList<Transition>();
+		this.transitions.add(t6);
+		t6.add(tran_6_4);
+		t6.add(tran_6_5);
+		List<Transition> t7 = new ArrayList<Transition>();
+		t7.add(tran_7_5);
+		this.transitions.add(t7);
 
 		this.currentState = this.initialState;
 	}
@@ -139,15 +169,22 @@ public class CalcultorFSM implements FiniteStateMachine, Runnable {
 	public String receive(String s) {
 		State cur = this.getCurrentState();
 		// get all condition
-		List<Transition> trans = this.transitions.get(index);
+		List<Transition> trans = this.transitions.get(this.states.indexOf(cur));
 		// check it
 		Transition tran = null;
+		for (Transition t : trans) {
+			Condition cond = t.getCondition();
+			if (cond != null && cond.isGuard(s)) {
+				tran = t;
+				break;
+			}
+		}
 		// get next state
 		if (tran != null) {
 
 		}
 		// make output
-		String out = ;
+		String out = "";
 		return out;
 	}
 }
