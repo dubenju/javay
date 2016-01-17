444cd36c1d8e5ba28397a9a3ea3f182c003b93e2
dubenju <dubenju@163.com>
2016-01-18 08:39:40 +0900

    关于自动更新的考虑

---
+++ src/java/javay/http/JavayHttp.java
@@ -0,0 +1,16 @@
+package javay.http;
+
+public class JavayHttp {
+	private String url = "";
+	public String getVersion() {
+		return "";
+	}
+	public boolean getChksum() {
+		boolean bRes = false;
+		return bRes;
+	}
+	public boolean getModule() {
+		boolean bRes = false;
+		return bRes;
+	}
+}
---
+++ src/java/javay/main/Launcher.java
@@ -0,0 +1,20 @@
+package javay.main;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		Launcher launcher = new Launcher();
+		launcher.run();
+	}
+
+	public void run() {
+		// 检查检查更新选项
+		// 检查版本
+		// 下载新版本
+		// 检查下载内容
+		// 解压
+		// jar更新
+		// 配置文件更新
+		// 启动
+	}
+}
