4493a97a171f88679f78cdae02157611a1d7062c
dubenju <dubenju@163.com>
2016-01-18 20:38:11 +0900

    MD5

---
+++ src/java/javay/test/http/MD5HashingExample.java
@@ -0,0 +1,33 @@
+package javay.test.http;
+
+import java.security.MessageDigest;
+
+public class MD5HashingExample 
+{
+    public static void main(String[] args)throws Exception
+    {
+    	String password = "123456";
+    	
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(password.getBytes());
+        
+        byte byteData[] = md.digest();
+ 
+        //convert the byte to hex format method 1
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < byteData.length; i++) {
+         sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+        }
+     
+        System.out.println("Digest(in hex format):: " + sb.toString());
+        
+        //convert the byte to hex format method 2
+        StringBuffer hexString = new StringBuffer();
+    	for (int i=0;i<byteData.length;i++) {
+    		String hex=Integer.toHexString(0xff & byteData[i]);
+   	     	if(hex.length()==1) hexString.append('0');
+   	     	hexString.append(hex);
+    	}
+    	System.out.println("Digest(in hex format):: " + hexString.toString());
+    }
+}
